04dc89d399a27086e1496e8185faad63e8114b46
Dave Davis <dgd.code@gmail.com>
2014-10-04 02:22:26 -0400

    Google Auth working.

--- srv/lib/config/routes.js
+++ srv/lib/config/routes.js
@@ -1,7 +1,40 @@
+/*jshint node:true, es3:false*/
+var passport = require('passport');
+
 module.exports = function (app, config) {
 
-  app.get('*', function (req, res) {
-    res.sendfile(config.webRoot.concat('/index.html'));
-  });
+  app.route('/auth/google')
+    .get(passport.authenticate('google', {scope: config.google.scope, response_type: 'token', include_granted_scopes: true}));
 
+  app.route('/oauth2callback')
+    .get(passport.authenticate('google', { failureRedirect: '/'}),
+    function (req, res) {
+      res.redirect('/');
+    }
+  );
+
+  app.route('/api/profile')
+    .get(function (req, res) {
+      if (req.user) {
+        res.json(req.user);
+      } else {
+        res.send(401);
+      }
+    });
+
+  app.route('/api/session')
+    .delete(function (req, res) {
+      req.logout();
+      res.send(200);
+    });
+
+/*
+  app.get('*', function (req, res) {
+    res.render('index');
+  });
+*/
+
+  app.get('*', function (req, res) {
+    res.sendFile(config.webRoot.concat('/index.html'));
+  });
 };
