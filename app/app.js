3c8a2a0edafd5054e4e23d796f898d85ef22e465
Dave Davis <dgd.code@gmail.com>
2014-10-05 03:02:33 -0400

    Added the default views

--- app/app.js
+++ app/app.js
@@ -2,31 +2,32 @@
 
   angular.module('GcloudDns', [
     'ui.router',
-    'ngMaterial',
     'xd.layout.PageHeader',
-    'xd.tmpls'
+    'xd.tmpls',
+    'xd.views.Welcome',
+    'xd.views.DnsManager'
   ])
     .config(config)
     .controller('gcloudDnsCtrl', GcloudDnsCtrl);
 
   /* @ngInject */
-  function config ($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
+  function config ($httpProvider, $stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-/*
-    $stateProvider
-      .state('home', { url: '/test1', templateUrl: '/views/test1/test1.html', controller: 'Test1Ctrl', controllerAs: 'vm'})
-      .state('test2', { url: '/test2', templateUrl: '/views/test2/test2.html', controller: 'Test2Ctrl', controllerAs: 'vm'});
 
-    $urlRouterProvider.otherwise('/test1');*/
+    $stateProvider
+      .state('welcome', { templateUrl: '/views/welcome/welcome.html', controller: 'welcomeCtrl', controllerAs: 'vm'})
+      .state('dns-manager', { templateUrl: '/views/dns-manager/dns-manager.html', controller: 'dnsManagerCtrl', controllerAs: 'vm'});
+
   }
 
   /* @ngInject */
-  function GcloudDnsCtrl () {
+  function GcloudDnsCtrl ($state) {
     var vm = this;
     vm.appTitle = 'Gcloud Dns';
+    $state.go('welcome');
   }
 
 
