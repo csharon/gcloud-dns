12ddb082deab881746c5d56488ceee07616e0fe2
Chris Sharon <chris@xdoji.com>
2014-11-13 02:53:22 -0500

    add support for multiple rrdata values

--- app/components/record-form/record-form.js
+++ app/components/record-form/record-form.js
@@ -27,23 +27,43 @@
   function RecordFormCtrl($scope, changeSetModel) {
     var vm = this;
     vm.record = angular.copy(changeSetModel.currentRecord);
-    if (!changeSetModel.currentRecordIsNew) {
-      vm.rrdata = vm.record.rrdatas[0];
-    } else {
-      vm.rrdata = '';
-    }
+    vm.addRRData = addRRData;
+    vm.removeRRData = removeRRData;
+    vm.rrdata = '';
+    vm.disableAddRRData = true;
 
     vm.save = save;
     vm.cancel = cancel;
 
+    function addRRData() {
+      if (!angular.isArray(vm.record.rrdatas)) {
+        vm.record.rrdatas = [];
+      }
+      vm.record.rrdatas.push(vm.rrdata);
+      vm.rrdata = '';
+    }
+
+    function removeRRData(index) {
+      vm.record.rrdatas.splice(index, 1);
+    }
+
     function save() {
-      vm.record.rrdatas = [vm.rrdata];
+      //vm.record.rrdatas = [vm.rrdata];
       $scope.$emit('SAVE_RECORD', vm.record);
     }
 
     function cancel() {
       $scope.$emit('CANCEL_EDIT_RECORD');
     }
+
+    $scope.$watch(
+      function () {
+        return vm.rrdata;
+      },
+      function (val) {
+        vm.disableAddRRData = _.isEmpty(val);
+      }
+    );
 
   }
 
