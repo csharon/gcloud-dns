d3a2d7ec18d56dce2a6b1d4dfe6a162cd82847fb
Chris Sharon <chris@xdoji.com>
2014-11-30 21:24:39 -0500

    add MXRecordSet that inherits from DNSRecordSet, MXRRDataValue that creates mx fields

---
+++ app/services/mx-record-set/mx-record-set-spec.js
@@ -0,0 +1,9 @@
+/*globals inject, beforeEach, describe, it, expect, module*/
+/*jshint expr: true*/
+describe('xd.services.MXRecordSet', function () {
+
+  beforeEach( module('xd.services.MXRecordSet'));
+
+  it('should have a data function that returns an array');
+
+});
---
+++ app/services/mx-rrdata-value/mx-rrdata-value-spec.js
@@ -0,0 +1,9 @@
+/*globals inject, beforeEach, describe, it, expect, module*/
+/*jshint expr: true*/
+describe('xd.services.MXRRDataValue', function () {
+
+  beforeEach( module('xd.services.MXRRDataValue'));
+
+  it('should have a data function that returns an array');
+
+});
